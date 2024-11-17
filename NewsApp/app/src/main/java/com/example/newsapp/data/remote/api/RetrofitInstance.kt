25231ddc5240474f475f1cb2e48e53ad856328b4
marcosimao12 <marcosimaocarvalho@gmail.com>
2024-11-17 23:35:07 +0000

    NewsAPP
    Update estrutura

---
+++ NewsApp/app/src/main/java/com/example/newsapp/data/remote/api/RetrofitInstance.kt
@@ -0,0 +1,16 @@
+package com.example.newsapp.data.remote.api
+
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object RetrofitInstance {
+    val api: NewsApi by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://newsapi.org/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+    }
+}
